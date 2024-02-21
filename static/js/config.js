57593c53453c7e63ee015b50ffe174579b330184
xsw <89456245+xswnb@users.noreply.github.com>
2024-02-21 16:26:07 +0800

    Update config.js
    api

--- static/js/config.js
+++ static/js/config.js
@@ -1,5 +1,5 @@
 const config = {
     // gpt3.5 官方接口：https://api.openai.com/v1/chat/completions
     url: "https://open.aiproxy.xyz/v1/chat/completions",  // gpt3.5代理接口
-    apiKey: ""  // openAi Base64 编码 apiKey
+    apiKey: "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"  // openAi Base64 编码 apiKey
 };  
